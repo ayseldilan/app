5dc3e28e3b7c66e23eb1c62112e2918463a23b98
Ayseldilan <83372761+ayseldilan@users.noreply.github.com>
2022-12-22 19:46:16 +0000

    adding nav section

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
+
 export default function Nav() {
     return(
-        <div>
-            <h1>Nav</h1>
-        </div>
+        <nav className="flex justify-between items-center py-10">
+            <Link href="/">
+                <button className="text-lg font-medium">Creative Minds</button>
+            </Link>
+            <ul className="flex items-center gap-10">
+                <Link href={"/auth/login"}>
+                    
+                    Back to Home
+                </Link>
+            </ul>
+
+
+        </nav>
     );
 }
